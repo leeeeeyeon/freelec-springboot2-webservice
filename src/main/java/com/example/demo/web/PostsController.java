dfd10f4b0c45ec453429a2ef7d0c5666dc4fcec3
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-06-21 18:26:03 +0900

    ☕️ 등록/수정/조회 API 구현

---
+++ src/main/java/com/example/demo/web/PostsController.java
@@ -0,0 +1,29 @@
+package com.example.demo.web;
+
+import com.example.demo.service.posts.PostsService;
+import com.example.demo.web.dto.PostsResDto;
+import com.example.demo.web.dto.PostsSaveReqDto;
+import com.example.demo.web.dto.PostsUpdateReqDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveReqDto reqDto) {
+        return postsService.save(reqDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateReqDto reqDto) {
+        return postsService.update(id, reqDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+}
