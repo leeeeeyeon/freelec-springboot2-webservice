dd0674635ed201be7acd5b7fb573a0bd4af11d0e
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-06-29 21:07:35 +0900

    ☕️ Spring Security와 OAuth 2.0을 활용한 구글 로그인 구현

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.config.auth.dto.SessionUser;
 import com.example.demo.web.dto.PostsResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,14 +10,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.example.demo.service.posts.PostsService;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
