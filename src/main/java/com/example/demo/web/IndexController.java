8e48870f7b174db18e2721e4ea0221d16e3b89ae
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-07-05 00:51:00 +0900

    🪛 어노테이션을 이용하여 SessionUser 리팩토링

--- src/main/java/com/example/demo/web/IndexController.java
+++ src/main/java/com/example/demo/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.demo.web;
 
+import com.example.demo.config.auth.LoginUser;
 import com.example.demo.config.auth.dto.SessionUser;
 import com.example.demo.web.dto.PostsResDto;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
